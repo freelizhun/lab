66db00c88a2efd82c4b0f46ffb2b6ef57c8e319d
root <root@ubuntu.(none)>
2013-03-20 00:00:03 -0700

    adding route

---
+++ route_refactor/test.py
@@ -0,0 +1,13 @@
+import urllib
+import httplib2
+
+url = 'http://localhost:8090/search?q=haha&aq=f'   
+#url = 'http://localhost:8090'   
+body = {'USERNAME': 'foo', 'PASSWORD': 'bar'}
+headers = {'Content-Type': 'application/json'}
+#headers = {'Content-Type': 'text/plane'}
+http = httplib2.Http()
+#Request.blank('/serarch?id=1')
+response, content = http.request(url, 'GET', headers=headers, body=urllib.urlencode(body))
+print response
+print content
