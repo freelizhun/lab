dc03cebc49d51eac5c7b98d0821daa84bac018fd
Jonah Wu - PTT <jonah.wu@tw.promise.com>
2014-10-02 16:22:43 +0800

    adding call in class

---
+++ classtest/call.py
@@ -0,0 +1,14 @@
+
+
+class Inout(object):
+    def __init__(self, haha=None):
+        if not haha:
+            self.haha = 'haha' 
+        else:
+            self.haha = haha
+    def __call__(self):
+        print self.haha
+
+print 'start run'
+a = Inout('aaa')
+a()
