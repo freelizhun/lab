3040e10e7d65bf6cbd003b1b9219fb353d900d5f
Jonah Wu - PTT <jonah.wu@tw.promise.com>
2014-12-25 12:35:24 +0800

    adding upload file

---
+++ golang/example/upload/u1.go
@@ -0,0 +1,28 @@
+package main
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+//curl -v -X POST -N --data-binary @"./haproxy.cfg" http://localhost:8080/webUploadHandler
+func webUploadHandler(w http.ResponseWriter, r *http.Request) {
+	out, err := os.Create("haha.txt")
+	if err != nil {
+		fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
+		return
+	}
+	defer out.Close()
+
+	// write the content from POST to the file
+	//_, err = io.Copy(out, file)
+	fmt.Println("start to copy")
+	io.Copy(out, r.Body)
+	fmt.Println("File uploaded successfully : ")
+
+}
+func main() {
+	http.HandleFunc("/", webUploadHandler)
+	http.ListenAndServe(":8080", nil)
+}
