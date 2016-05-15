399560e97e8e9df324cea6e00b3e5bc8ac27bbef
junmein <junmein@junmeindeMacBook-Pro.local>
2016-05-15 19:14:02 +0800

    adding access request information

--- pecan/pecan_test/test_project/test_project/controllers/v2/query2.py
+++ pecan/pecan_test/test_project/test_project/controllers/v2/query2.py
@@ -11,6 +11,7 @@ import commands
 class NextController(rest.RestController):
     _custom_actions = {
         'name': ['GET'],
+        'methodget': ['GET','POST'],
     }
     def __init__(self, user_id=None):
         print 'into next init'
@@ -20,6 +21,22 @@ class NextController(rest.RestController):
     @pecan.expose()
     def get(self):
         return 'next controller get routing'
+
+    @pecan.expose()
+    def methodget(self):
+        #the pecan.request function is in pecan.Request, so strnge
+        #the default body must in method of POST
+        print 'the header is %s'%pecan.request.headers.get('testheader')
+        print 'the method is %s'%pecan.request.method
+        print 'the data is %s'%pecan.request.body
+        print 'the host is %s'%pecan.request.host
+        print 'the param is %s'%pecan.request.params.get('aaa')
+        print 'the query is %s'%pecan.request.query_string
+        print 'the path is %s'%pecan.request.path
+        print 'the path_info is %s'%pecan.request.path_info
+        print 'the path_qs is %s'%pecan.request.path_qs
+        
+        return 'get method ok'
 
     @pecan.expose()
     def name(self):
